c6ec241b842e2c39f7d6bb6dc5163890a69b8c5a
elmara16 <elmara16@ru.is>
2018-10-26 11:12:09 +0000

    fixing conflict made by me

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,6 @@
 const turnFunctions = require("../logic/playerturn");
-<<<<<<< HEAD
 const playAgainButton = require("../logic/playAgain");
-=======
 const winning = require("../logic/winning");
->>>>>>> 0dd45ad9addee737d633bebf7a180913a8307088
 $(document).ready(function() {
 
     // DOM Objects
@@ -12,10 +9,7 @@ $(document).ready(function() {
     var resetButton = $('#reset-button');
     var xScoreText = $('#x-score');
     var oScoreText = $('#o-score');
-<<<<<<< HEAD
-=======
 
->>>>>>> 0dd45ad9addee737d633bebf7a180913a8307088
     // Variables
     var player = "X";
     var gameOver = false;
@@ -23,10 +17,8 @@ $(document).ready(function() {
     var winner = "";
     var xScore = 0;
     var oScore = 0;
-<<<<<<< HEAD
     var counter = 0;
-=======
->>>>>>> 0dd45ad9addee737d633bebf7a180913a8307088
+
 
 	// Board click event
     board.on('click', function() {
@@ -40,12 +32,9 @@ $(document).ready(function() {
 		    console.log("Winner is " + winner);
 		}
     });
-<<<<<<< HEAD
 
     resetButton.on('click', function () {
       playAgainButton.playAgain(xScore, oScore, player, counter);
     });
 
-=======
->>>>>>> 0dd45ad9addee737d633bebf7a180913a8307088
 });
