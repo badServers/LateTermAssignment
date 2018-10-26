5d658d04df90fbf4c4bfad843a1547305bf81262
aSnorri <adalberg17@ru.is>
2018-10-26 16:28:49 +0000

    Added playerturns and winning logic to gameboard

--- src/logic/winning.test.js
+++ src/logic/winning.test.js
@@ -1,10 +1,10 @@
 const winning = require('./winning');
 
 describe("isWinner", () => {
-  const expected = ["X","X","",
+  const expected = ["X","X","X",
                     "O","O","",
                     "","",""];
   it("Should return an empty string", () => {
-    expect(winning.isWinner(expected,"X",false)).toBe("");
+    expect(winning.isWinner(expected,"X",false)).toBe("X");
   });
 });
