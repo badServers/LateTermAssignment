c09c6e92aed38ff313beee68d93b7c1f58e811a7
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2018-10-26 18:38:42 +0000

    third row horizontal x test with a function

--- src/logic/winning.test.js
+++ src/logic/winning.test.js
@@ -17,5 +17,12 @@ X_winner_horizontal(testing_first_row_horizontal_X);
 var testing_second_row_horizontal_X =
 	       ["O", "X", "O",
 	        "X", "X", "X",
-	        "O", "O", "X"];
+	        "O", "O", "X"
+	       ];
 X_winner_horizontal(testing_second_row_horizontal_X);
+
+var testing_third_row_horizontal_X = 
+	       ["O", "X", "X",
+	        "X", "O", "O",
+	        "X", "X", "X"];
+X_winner_horizontal(testing_third_row_horizontal_X);
