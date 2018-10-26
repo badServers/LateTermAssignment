5a8d31bc403bc614429cbed1bd4e1ef34d9cff7d
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2018-10-26 16:23:22 +0000

    test for X turn in logic

--- src/logic/playerturn.test.js
+++ src/logic/playerturn.test.js
@@ -1,7 +1,10 @@
 const turnFunctions = require('./playerturn');
 
 describe("Change player", () => {
-	it("Should return opposite player", () => {
-		expect(turnFunctions.changePlayer("X")).toBe("O");
+	it("Should return the O player", () => {
+	     expect(turnFunctions.changePlayer("X")).toBe("O");
+	});
+	it("Should return the X player", () => {
+	     expect(turnFunctions.changePlayer('O')).toBe("X");
 	});
 });
