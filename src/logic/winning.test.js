2a562b7716338c4f72d97399fcacad625b50f575
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2018-10-26 18:53:59 +0000

    Is x a winner horizontal first row? - testing the function

--- src/logic/winning.test.js
+++ src/logic/winning.test.js
@@ -1,13 +1,23 @@
 const winning = require('./winning');
+//Horizontal winner
 function X_winner_horizontal(expected){
    describe("Is X a winner horizontal?", () => {
-     it("Should return an X (testing horizontal)", () => 
+     it("Should return an X (testing X horizontal)", () => 
        {
          expect(winning.isWinner(expected, "X",false)).toBe("X");
        });
    });
 }
 
+function X_winner_vertically(expected){
+   describe("Is X a winner horizontal?", () => {
+      it("Should return an X (testing X vertically)", () =>
+        {
+	  expect(winning.isWinner(expected, "X", false)).toBe("X");
+        });
+   });
+}
+//Horizontal testing
 var testing_first_row_horizontal_X = 
 	       ["X", "X", "X",
 	         "O", "O", "X",
@@ -26,5 +36,15 @@ X_winner_horizontal(testing_second_row_horizontal_X);
 var testing_third_row_horizontal_X = 
 	       ["O", "X", "X",
 	        "X", "O", "O",
-	        "X", "X", "X"];
+	        "X", "X", "X"
+	       ];
 X_winner_horizontal(testing_third_row_horizontal_X);
+//Horizontal testing finished
+
+//Vertical testing
+var testing_first_row_vertically_X = 
+	       ["X", "X", "O",
+		"X", "O", "O",
+		"X", "O", "X"
+	       ];
+X_winner_vertically(testing_first_row_vertically_X);
