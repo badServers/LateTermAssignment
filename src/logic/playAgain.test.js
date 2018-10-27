5e148d40640be273d4e8ae0ed7428929373dbfff
elmara16 <elmara16@ru.is>
2018-10-27 14:39:10 +0000

    refactor delete playagain & greeting

--- src/logic/playAgain.test.js
+++
@@ -1,8 +0,0 @@
-const playAgainButton = require('./playAgain');
-global.window = window
-global.$ = require('jquery');
-describe("Resest game", () => {
-	it("Should return player = X", () => {
-		expect(playAgainButton.playAgain(5, 2, 'O', 3)).toBe("X");
-	});
-});
