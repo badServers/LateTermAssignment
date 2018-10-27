cb6aabfd74833a674c63ee6b14d8c81671cc405e
Hilmar96 <hilmar96@gmail.com>
2018-10-27 14:17:58 +0000

    Winner check change

--- src/logic/winning.js
+++ src/logic/winning.js
@@ -1,45 +1,23 @@
-function isWinner(board, player, gameOver, round) {
-  if(board[0] === player && board[1] === player && board[2] === player) {
-    gameOver = true;
-    return gameOver;
-  }
-  else if(board[3] === player && board[4] === player && board[5] === player) {
-    gameOver = true;
-    return gameOver;
-  }
-  else if(board[6] === player && board[7] === player && board[8] === player) {
-    gameOver = true;
-    return gameOver;
-  }
-  else if(board[0] === player && board[3] === player && board[6] === player) {
-    gameOver = true;
-    return gameOver;
-  }
-  else if(board[1] === player && board[4] === player && board[7] === player) {
-    gameOver = true;
-    return gameOver;
-  }
-  else if(board[2] === player && board[5] === player && board[8] === player) {
-    gameOver = true;
-    return gameOver;
-  }
-  else if(board[0] === player && board[4] === player && board[8] === player) {
-    gameOver = true;
-    return gameOver;
-  }
-  else if(board[2] === player && board[4] === player && board[6] === player) {
-    gameOver = true;
-    return gameOver;
-  }
-  else if(round === 9) {
-	gameOver = true;
-	return gameOver;
-  }
-  else {
-    return false;
-  }
+function isWinner(board, player, round) {
+    if(board[0] === player && board[1] === player && board[2] === player ||
+       board[3] === player && board[4] === player && board[5] === player ||
+       board[6] === player && board[7] === player && board[8] === player ||
+       board[0] === player && board[3] === player && board[6] === player ||
+       board[1] === player && board[4] === player && board[7] === player ||
+       board[2] === player && board[5] === player && board[8] === player ||
+       board[0] === player && board[4] === player && board[8] === player ||
+       board[2] === player && board[4] === player && board[6] === player
+    ) {
+        return { "winner": player };
+    }
+    else if (round == 9) {
+        return { "winner": "Draw" };
+    }
+    else {
+        return { "winner": "" };
+    }
 }
 
 module.exports = {
-  isWinner
+    isWinner
 }
