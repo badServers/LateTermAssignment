76df6ccea5cb1b51a5ce2f9e1ce9343d8525a60c
Hilmar96 <hilmar96@gmail.com>
2018-10-27 15:53:04 +0000

    Some tests and cleanup of code

--- src/server/api.test.js
+++ src/server/api.test.js
@@ -1,17 +1,30 @@
 const request = require("supertest");
 const app = require("../../app");
 
-describe("OPTIONS /api", () => {
-  it("should return 200 status code", async () => {
-    const res = await request(app).options("/api");
-    expect(res.status).toBe(200);
-  });
+describe("GET /api/game/gameboard/status", () => {
+    it("Should return a 200 status code", async () => {
+        const res = await request(app).get("/api/game/gameboard/status");
+        expect(res.status).toBe(200);
+    });
 });
 
-describe("GET /api", () => {
-  it("should return 405 status code and an error message", async () => {
-    const res = await request(app).get("/api");
-    expect(res.status).toBe(405);
-    expect(res.body).toHaveProperty("error");
-  });
+describe("GET /api/game/gameboard/updateBoard/:tile", () => {
+    it("Should return a 200 status code", async () => {
+        const res = await request(app).get("/api/game/gameboard/updateBoard/0");
+        expect(res.status).toBe(200);
+    });
+});
+
+describe("GET /api/game/gameboard/resetBoard", () => {
+    it("Should return a 200 status code", async () => {
+        const res = await request(app).get("/api/game/gameboard/resetBoard");
+        expect(res.status).toBe(200);
+    });
+});
+
+describe("GET /api/game/gameboard/updateMessage", () => {
+    it("Should return a 200 status code", async () => {
+        const res = await request(app).get("/api/game/gameboard/updateMessage");
+        expect(res.status).toBe(200);
+    });
 });
